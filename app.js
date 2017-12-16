8aa30dd65f2ef17bebc44b058bb37cb62854d054
Tristan Homsi <tristanhomsi@gmail.com>
2017-12-15 20:09:22 -0500

    procz

--- app.js
+++ app.js
@@ -13,6 +13,24 @@ var parseUrl = function(url) {
     return url;
 };
 
+async function screenshotDOMElement(page, selector, padding = 0) {
+  const rect = await page.evaluate(selector => {
+    const element = document.querySelector(selector);
+    const {x, y, width, height} = element.getBoundingClientRect();
+    return {left: x, top: y, width, height, id: element.id};
+  }, selector);
+
+  return await page.screenshot({
+    path: 'element.png',
+    clip: {
+      x: rect.left - padding,
+      y: rect.top - padding,
+      width: rect.width + padding * 2,
+      height: rect.height + padding * 2
+    }
+  });
+}
+
 app.get('/', function(req, res) {
     var urlToScreenshot = parseUrl(req.query.url);
 
@@ -24,8 +42,9 @@ app.get('/', function(req, res) {
             });
 
             const page = await browser.newPage();
+            await page.setViewport({width: 640, height: 800, deviceScaleFactor: 2});
             await page.goto(urlToScreenshot);
-            await page.screenshot().then(function(buffer) {
+            await screenshotDOMElement(page, ".highlight-detail-passage").then(function(buffer) {
                 res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
                 res.setHeader('Content-Type', 'image/png');
                 res.send(buffer)
